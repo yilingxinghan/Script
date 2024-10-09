bb12fe51b335aed2e1298cdbd3678abe772a71f1
零寒 <149072768+yilingxinghan@users.noreply.github.com>
2024-10-09 20:33:43 +0800

    Update connect.js

--- connect.js
+++ connect.js
@@ -73,19 +73,3 @@ var WSNRTConnect = function() {
         thiz.wsc.send(dat);
     };
 };
-
-var conn = new WSNRTConnect();
-conn.setId('893058783207')
-conn.setKey('CQ8DBAwPBg0ACgIAQxtFUVlRU1FHEAoXCQABAAYLBg0AAhwXGldSXVAaDRgRXllCTFZdVxdFAQ');
-conn.setMac('01:12:4B:00:08:8A:42:59');
-conn.setServerAddr('api.zhiyun360.com:28080');
-conn.onConnect = function() {
-    console.log("连接成功!");
-};
-conn.onConnectLost = function() {
-    console.log("连接丢失!");
-};
-conn.onmessageArrive = function(msg) {
-    console.log(msg);
-};
-conn.connect();
